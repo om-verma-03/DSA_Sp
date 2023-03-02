a58294f9efd3a21dafac8f1b5445a966bcc4deb9
om-verma-03 <omverma1219@gmail.com>
2023-03-02 21:19:13 +0530

    Aggressive Cows

---
+++ Searching/Aggressive_Cows.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& stalls,int k,int mid){
+        //can we place k cows, with at least k distance between cows
+        
+        int c = 1;
+        int pos = stalls[0];
+        
+        for(int i=0;i<stalls.size();i++){
+            if(stalls[i] - pos >= mid){
+                c++;
+                pos = stalls[i];
+            }
+            if(c == k){
+                return true;
+            }
+        }
+        return false;
+    }
+    int solve(int n, int k, vector<int> &stalls) {
+        sort(stalls.begin(),stalls.end());
+        int start = 0;
+        int end = stalls[stalls.size()-1] - stalls[0];
+        
+        int ans = -1;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(isPossible(stalls,k,mid)){
+                ans = mid;
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
