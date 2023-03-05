cdbbd528b8367c4548c1398af498ea7a49e86e26
om-verma-03 <omverma1219@gmail.com>
2023-03-05 16:51:49 +0530

    647. Palindromic Substrings

---
+++ Char_Strings/Palindromic_Substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int ExpandAroundIndex(string s,int i,int j){
+        int count = 0;
+        while(i >= 0 && j < s.size() && s[i] == s[j]){
+            for(int temp=i;temp<=j;temp++){
+                cout<<s[temp]<<" ";
+            }cout<<endl;
+
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int count = 0;
+
+        for(int i=0;i<s.size();i++){
+            //Odd
+            cout<<"Odd: "<<endl;
+            int oddAns = ExpandAroundIndex(s,i,i);
+            count = count + oddAns;
+
+            //Even
+            cout<<"Even: "<<endl;
+            int evenAns = ExpandAroundIndex(s,i,i+1);
+            count = count + evenAns;
+        }
+        return count;
+    }
+};
