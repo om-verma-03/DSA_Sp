f18de42256c8eb9500f53de11e46b7a1ef8b535a
om-verma-03 <omverma1219@gmail.com>
2023-03-15 21:16:53 +0530

    Sieve of Eratosthenes

---
+++ Basic_Mathematics/Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int Sieve(int n){
+    int count = 0;
+
+    vector<bool> primes(n+1,true);
+
+    primes[0] = false;
+    primes[1] = false;
+
+    for(int i=0;i<=n;i++){
+        if(primes[i] == true){
+            count++;
+
+            for(int j=i;j<=n;j+=i){
+                primes[j] = false;
+            }
+        }
+    }
+    return count;
+}
+int main(){
+    int count = Sieve(5);
+    cout<<"Count of total primes till n is: "<<count; 
+    return 0;
+}
