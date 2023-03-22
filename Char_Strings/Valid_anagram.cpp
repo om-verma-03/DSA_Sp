de4ec540ae97afc2f01cd9126bcad1ae5bcf8206
om-verma-03 <omverma1219@gmail.com>
2023-03-22 12:00:06 +0530

    242. Valid Anagram

--- Char_Strings/Valid_anagram.cpp
+++ Char_Strings/Valid_anagram.cpp
@@ -22,3 +22,26 @@ public:
         return false;
     }
 };
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int hash[256] = {0};
+
+        for(int i=0;i<s.size();i++){
+            hash[s[i]]++;
+        }
+
+        for(int i=0;i<t.size();i++){
+            hash[t[i]]--;
+        }
+
+        for(int i=0;i<256;i++){
+            if(hash[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
